af4256a5a88428f351a1726b74cd2213466315c2
samnewga <newgate90s@gmail.com>
2020-04-09 23:43:36 -0700

    before detour

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,25 @@
+INSERT INTO blogful_articles
+    (title, date_published, content)
+VALUES
+    ('article 1', '2020-01-22 19:10:25-07', 'article 1 text' ),
+    ('article 2', '2020-02-22 19:10:25-07', 'article 2 text' ),
+    ('article 3', '2020-03-22 19:10:25-07', 'article 3 text' ),
+    ('article 4', '2020-04-22 19:10:25-07', 'article 4 text' ),
+    ('article 5', '2020-05-22 19:10:25-07', 'article 5 text' ),
+    ('article 6', '2020-06-22 19:10:25-07', 'article 6 text' ),
+    ('article 7', '2020-07-22 19:10:25-07', 'article 7 text' ),
+    ('article 8', '2020-08-22 19:10:25-07', 'article 8 text' ),
+    ('article 9', '2020-09-22 19:10:25-07', 'article 9 text' ),
+    ('article 10', '2020-10-22 19:10:25-07', 'article 10 text' ),
+    ('article 11', '2020-11-22 19:10:25-07', 'article 11 text' ),
+    ('article 12', '2020-12-22 19:10:25-07', 'article 12 text' ),
+    ('article 13', '2020-01-01 19:10:25-07', 'article 13 text' ),
+    ('article 14', '2020-02-02 19:10:25-07', 'article 14 text' ),
+    ('article 15', '2020-03-03 19:10:25-07', 'article 15 text' ),
+    ('article 16', '2020-04-04 19:10:25-07', 'article 16 text' ),
+    ('article 17', '2020-05-05 19:10:25-07', 'article 17 text' ),
+    ('article 18', '2020-06-06 19:10:25-07', 'article 18 text' ),
+    ('article 19', '2020-07-07 19:10:25-07', 'article 19 text' ),
+    ('article 20', '2020-08-08 19:10:25-07', 'article 20 text' )
+,
+;
---
+++ test/articles-service.spec.js
@@ -0,0 +1,19 @@
+const ArticlesService = require('../src/articles-service');
+const knex = require('knex');
+
+let db;
+
+before(() => {
+  db = knex({
+    client: 'pg',
+    connection: process.env.TEST_DB_URL,
+  });
+});
+
+describe(`Articles service object`, function () {
+  describe(`getAllArticles()`, () => {
+    it(`resolves all articles from 'blogful_articles' table`, () => {
+      // test that ArticlesService.getAllArticles gets data from table
+    });
+  });
+});
